819863c1f82bd93fac76fadb881d139cf75fe287
Firsov62121 <53371833+Firsov62121@users.noreply.github.com>
2020-06-18 17:22:25 +0300

    8

--- app/src/main/java/com/example/kvantnews/MainActivity.java
+++ app/src/main/java/com/example/kvantnews/MainActivity.java
@@ -2,15 +2,27 @@ package com.example.kvantnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     EditText password_et, login_et;
+    ImageView main_background;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         password_et = findViewById(R.id.user_password_et);
         login_et = findViewById(R.id.user_login_et);
+        main_background = (ImageView) findViewById(R.id.login_main_iv);
+        new ChangeBackgroundIfExist().execute();
 
         ((Button)findViewById(R.id.create_account_btn)).setOnClickListener(this);
         ((Button)findViewById(R.id.login_btn)).setOnClickListener(this);
@@ -34,11 +48,126 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(intent);
                 break;
             case R.id.login_btn:
-                //
+                if(isOnline()){
+                    String login = login_et.getText().toString();
+                    String password = password_et.getText().toString();
+                    String type;
+                    try {
+                         type = new IsOkUser(login, password, this).execute().get();
+                    }catch (Exception e) {
+                        type = "fail";
+                    }
+                }else{
+                    Toast.makeText(this, R.string.no_internet_connection, Toast.LENGTH_SHORT).show();
+                }
                 break;
             case R.id.remind_password_btn:
-                //
+                if(isOnline()){
+                    String login = login_et.getText().toString();
+                    if(login.length() > 4 && login.length() < 20){
+                        new RemindPassword(login, this).execute();
+                    }else{
+                        Toast.makeText(this, R.string.enter_correct_password, Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(this, R.string.no_internet_connection, Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
+    }
+
+    public static boolean isOnline() {
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            Process ipProcess = runtime.exec("/system/bin/ping -c 1 8.8.8.8");
+            int     exitValue = ipProcess.waitFor();
+            return (exitValue == 0);
+        }
+        catch (IOException | InterruptedException e)          { e.printStackTrace(); }
+        return false;
+    }
+
+    static class RemindPassword extends AsyncTask<String, String, String>{
+        String login;
+        Context context;
+
+        RemindPassword(String login, Context context){
+            this.login = login;
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String res = "fail";
+
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s.equals("ok")){
+                Toast.makeText(context, R.string.password_remind_sended, Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(context, R.string.its_fail, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    static class IsOkUser extends AsyncTask<String, String, String>{
+        String login, password;
+        Context context;
+
+        IsOkUser(String login, String password, Context context){
+            this.login = login;
+            this.password = password;
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String res = "fail";
+
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(!s.equals("fail")){
+                Toast.makeText(context, R.string.please_wait, Toast.LENGTH_LONG).show();
+            }else{
+                Toast.makeText(context, R.string.its_fail, Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    class ChangeBackgroundIfExist extends AsyncTask<String, String, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String res = "fail";
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(!s.equals("fail")){
+                Bitmap newBackground = imageFromString64(s);
+                if(newBackground != null) {
+                    main_background.setImageBitmap(newBackground);
+                }
+            }
+        }
+    }
+
+    public static Bitmap imageFromString64(String image64){
+        if(!image64.equalsIgnoreCase("")){
+            byte[] imageBytes = Base64.decode(image64, Base64.DEFAULT);
+            InputStream is = new ByteArrayInputStream(imageBytes);
+            return BitmapFactory.decodeStream(is);
+        }
+        return null;
     }
 }
--- app/src/main/java/com/example/kvantnews/RegistrationActivity.java
+++ app/src/main/java/com/example/kvantnews/RegistrationActivity.java
@@ -2,15 +2,107 @@ package com.example.kvantnews;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegistrationActivity extends AppCompatActivity {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegistrationActivity extends AppCompatActivity implements View.OnClickListener {
+    EditText password_et, login_et, mail_et, password_repeat_et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        password_et = (EditText) findViewById(R.id.user_password_et);
+        password_repeat_et = (EditText) findViewById(R.id.user_password_repeat_et);
+        login_et = (EditText) findViewById(R.id.user_login_et);
+        mail_et = (EditText) findViewById(R.id.user_mail_et);
+
+        ((Button)findViewById(R.id.login_btn)).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login_btn:
+                if(MainActivity.isOnline()) {
+                    String mail = mail_et.getText().toString();
+                    String login = login_et.getText().toString();
+                    String password = password_et.getText().toString();
+                    String password_repeat = password_repeat_et.getText().toString();
+                    if (isEmailValid(mail)) {
+                        if (login.length() > 4 && login.length() < 20) {
+                            if (password.length() > 4 && password.length() < 16) {
+                                if (password.equals(password_repeat)) {
+                                    Toast.makeText(this, R.string.please_wait, Toast.LENGTH_SHORT).show();
+                                    new RegisterUser(password, login, mail, this).execute();
+                                } else {
+                                    Toast.makeText(this, R.string.passwords_should_equals, Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(this, R.string.lenght_of_password, Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(this, R.string.lenght_of_login, Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(this, R.string.failed_email, Toast.LENGTH_SHORT).show();
+                    }
+                }else{
+                    Toast.makeText(this, R.string.no_internet_connection, Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+        }
+    }
+
+    public boolean isEmailValid(String email) {
+        String regExpn = "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@" +
+                "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?" +
+                "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\." +
+                "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?" +"[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|" +
+                "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+        Pattern pattern = Pattern.compile(regExpn,Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    static class RegisterUser extends AsyncTask<String, String, String>{
+        String password, login, mail;
+        Context context;
+
+        RegisterUser(String password, String login, String mail, Context context){
+            this.mail = mail;
+            this.password = password;
+            this.login = login;
+            this.context = context;
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String res = "fail";
+
+            return res;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            if(s.equals("ok")){
+                Toast.makeText(context, R.string.you_are_successfully_registrated, Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(context, R.string.its_fail, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
